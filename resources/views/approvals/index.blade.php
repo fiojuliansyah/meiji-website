10c44b26982eddcb8daa4b174592d20ea05608b0
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-02-23 22:21:10 +0700

    Update index.blade.php

--- resources/views/approvals/index.blade.php
+++ resources/views/approvals/index.blade.php
@@ -40,22 +40,23 @@
                                     <td>
                                         @foreach ($content->requiredApprovals as $requirement)
                                             <div>
-                                                {{ $requirement->approvalType->name }}
-                                                @php
-                                                    $approval = $content->approvals->firstWhere('approval_type_id', $requirement->approval_type_id);
-                                                @endphp
-                                    
-                                                @if ($approval)
-                                                    @if ($approval->status === 'approved')
-                                                        <span class="badge bg-success">{{ translate('Approved') }}</span>
-                                                        <p class="text-muted mt-1"><small>{{ $approval->created_at->format('d m Y') }}</small></p>
-                                                    @elseif ($approval->status === 'rejected')
-                                                        <span class="badge bg-danger">{{ translate('Rejected') }}</span>
-                                                        <p class="text-muted mt-1"><small>{{ $approval->rejection_description }}</small></p>
+                                                <p>{{ $requirement->approvalType->user->name }} 
+                                                    <small>({{ $requirement->approvalType->name }}) </small>
+                                                    @php
+                                                        $approval = $content->approvals->firstWhere('approval_type_id', $requirement->approval_type_id);
+                                                    @endphp
+                                        
+                                                    @if ($approval)
+                                                        @if ($approval->status === 'approved')
+                                                            <span class="badge bg-success">{{ translate('Approved') }}</span>
+                                                        @elseif ($approval->status === 'rejected')
+                                                            <span class="badge bg-danger">{{ translate('Rejected') }}</span>
+                                                            <p class="text-muted mt-1"><small>{{ $approval->rejection_description }}</small></p>
+                                                        @endif
+                                                    @else
+                                                        <span class="badge bg-warning">{{ translate('Pending') }}</span>
                                                     @endif
-                                                @else
-                                                    <span class="badge bg-warning">{{ translate('Pending') }}</span>
-                                                @endif
+                                                </p>
                                             </div>
                                         @endforeach
                                     </td>                                                                 

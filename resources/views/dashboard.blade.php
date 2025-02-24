fb6fcf5fabd6681d5ef410f43ccd808da653939c
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-02-24 15:38:55 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,6 @@
                     <div class="page-header-title">
                         <h5 class="m-b-10">Dashboard</h5>
                     </div>
-                    <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="route">Home</a></li>
-                        <li class="breadcrumb-item">Dashboard</li>
-                    </ul>
                 </div>
             </div>
             <div class="main-content">

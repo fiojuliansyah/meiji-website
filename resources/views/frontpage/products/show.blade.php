eb17e5fb96e935342ff445d93efbabc7771836a9
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-02-28 01:18:05 +0700

    Update show.blade.php

--- resources/views/frontpage/products/show.blade.php
+++ resources/views/frontpage/products/show.blade.php
@@ -5,97 +5,100 @@
 @endsection
 
 @push('css')
-  <style>
-    /* Kelas untuk efek blur pada background */
-    .modal-blur-background {
-        filter: blur(5px); /* Sesuaikan tingkat blur sesuai kebutuhan */
-        transition: filter 0.3s ease;
-    }
-  </style>
+<style>
+  /* Kelas untuk efek blur */
+.modal-blur-background {
+    filter: blur(5px); /* Sesuaikan tingkat blur sesuai kebutuhan */
+    transition: filter 0.3s ease;
+}
+
+</style>
 @endpush
 
 @section('content')
-  <section class="page-title page-title-blank bg-white" id="page-title">
-      <div class="container">
-          <div class="row">
-              <div class="col-12 col-lg-6">
-                  <div class="title">
-                      <ol class="breadcrumb breadcrumb-long">
-                          <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
-                          <li class="breadcrumb-item">
-                              <a href="{{ route('frontpage.products.category', ['lang' => app()->getLocale(), 'slug' => $product->category->getTranslation('slug', app()->getLocale())]) }}">
-                                  {{ $product->category->getTranslation('name', app()->getLocale()) ?? 'Category' }}
-                              </a>
-                          </li>
-                          <li class="breadcrumb-item active" aria-current="page">{{ Illuminate\Support\Str::limit(strip_tags($product->getTranslation('name', app()->getLocale())), 20) }}</li>
-                      </ol>
-                  </div>
+<section class="page-title page-title-blank bg-white" id="page-title">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <div class="title">
+                    <ol class="breadcrumb breadcrumb-long">
+                        <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                        <li class="breadcrumb-item">
+                            <a href="{{ route('frontpage.products.category', ['lang' => app()->getLocale(), 'slug' => $product->category->getTranslation('slug', app()->getLocale())]) }}">
+                                {{ $product->category->getTranslation('name', app()->getLocale()) ?? 'Category' }}
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ Illuminate\Support\Str::limit(strip_tags($product->getTranslation('name', app()->getLocale())), 20) }}</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="single-product" id="single-product">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <div class="product-img"><img class="img-fluid" src="{{ asset('storage/' . $product->image) }}" alt="product image"/><a class="img-popup" href="{{ asset('storage/' . $product->image) }}" alt="product image"></a></div>
+          <!-- .product-img end-->
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="product-content">
+            <div class="product-title">
+              <h3>{{ $product->getTranslation('name', app()->getLocale()) ?? 'Product Name' }}</h3>
+            </div>
+            <div class="product-tabs">
+              <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation"><a class="active" href="#description" data-bs-target="#description" aria-controls="description" role="tab" data-bs-toggle="tab" aria-selected="true">{{ translate('description') }}</a></li>
+              </ul>
+              <div class="tab-content">
+                <div class="tab-pane active" id="description" role="tabpanel">
+                    {!! $product->getTranslation('content', app()->getLocale()) ?? 'Product Content' !!}
+                </div>
               </div>
+            </div>
           </div>
+        </div>
       </div>
+    </div>
   </section>
-
-  <section class="single-product" id="single-product">
-      <div class="container">
-          <div class="row">
-              <div class="col-12 col-lg-6">
-                  <div class="product-img"><img class="img-fluid" src="{{ asset('storage/' . $product->image) }}" alt="product image"/><a class="img-popup" href="{{ asset('storage/' . $product->image) }}" alt="product image"></a></div>
-              </div>
-              <div class="col-12 col-lg-6">
-                  <div class="product-content">
-                      <div class="product-title">
-                          <h3>{{ $product->getTranslation('name', app()->getLocale()) ?? 'Product Name' }}</h3>
-                      </div>
-                      <div class="product-tabs">
-                          <ul class="nav nav-tabs" role="tablist">
-                              <li role="presentation"><a class="active" href="#description" data-bs-target="#description" aria-controls="description" role="tab" data-bs-toggle="tab" aria-selected="true">{{ translate('description') }}</a></li>
-                          </ul>
-                          <div class="tab-content">
-                              <div class="tab-pane active" id="description" role="tabpanel">
-                                  {!! $product->getTranslation('content', app()->getLocale()) ?? 'Product Content' !!}
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
+  <section class="shop shop-2"> 
+    <div class="container"> 
+      <div class="row"> 
+        <div class="col-12"> 
+          <h5>{{ translate('related products') }}</h5>
+        </div>
       </div>
-  </section>
-
-  <section class="shop shop-2">
-      <div class="container">
-          <div class="row">
-              <div class="col-12">
-                  <h5>{{ translate('related products') }}</h5>
-              </div>
-          </div>
-          <div class="row">
-              @foreach ($related_products as $related)
-                  <div class="col-12 col-md-6 col-lg-3">
-                      <div class="product-item" data-hover="">
-                          <div class="product-img"><img src="{{ asset('storage/' . $related->image) }}" alt="Product" style="width: 200px; height: 200px; object-fit: cover;"/><a class="add-to-cart" href="{{ route('frontpage.products.show', [
-                              'lang' => app()->getLocale(),
-                              'category_slug' => $related->category->getTranslation('slug', app()->getLocale()),
-                              'products_slug' => $related->getTranslation('slug', app()->getLocale())
-                          ]) }}">{{ translate('read more') }}</a>
-                          </div>
-                          <div class="product-content">
-                              <div class="product-title"><a href="shop-single.html">{{ $related->getTranslation('name', app()->getLocale()) ?? 'Product Name' }}</a></div>
-                          </div>
-                      </div>
-                  </div>
-              @endforeach
-          </div>
+      <div class="row"> 
+        @foreach ($related_products as $related)    
+            <div class="col-12 col-md-6 col-lg-3">
+            <div class="product-item" data-hover="">
+                <div class="product-img"><img src="{{ asset('storage/' . $related->image) }}" alt="Product" style="width: 200px; height: 200px; object-fit: cover;"/><a class="add-to-cart" href="{{ route('frontpage.products.show', [
+                    'lang' => app()->getLocale(),
+                    'category_slug' => $related->category->getTranslation('slug', app()->getLocale()),
+                    'products_slug' => $related->getTranslation('slug', app()->getLocale())
+                ]) }}">{{ translate('read more') }}</a>
+                <div class="badge"></div>
+                </div>
+                <div class="product-content">
+                <div class="product-title"><a href="shop-single.html">{{ $related->getTranslation('name', app()->getLocale()) ?? 'Product Name' }}</a></div>
+                </div>
+            </div>
+            <!-- .product end-->
+            </div>
+        @endforeach
       </div>
+    </div>
   </section>
 @endsection
 
 @section('modal')
-<div class="modal fade" id="disclaimerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="disclaimerModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg modal-dialog-centered modalblur">
+<div class="modal fade modalblur" id="disclaimerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="disclaimerModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
       <div class="modal-content">
           <div class="modal-header">
               <h5 class="modal-title" id="disclaimerModalLabel">{{ translate('Important Disclaimer') }}</h5>
+              <!-- Tombol close dihapus karena tidak boleh menutup modal tanpa pilihan -->
           </div>
           <div class="modal-body p-0">
               <div class="card border-0 m-0">
@@ -111,9 +114,15 @@
                               @if ($general && $general->logo)
                                   <img class="logo logo-dark" src="{{ asset('storage/' . $general->logo) }}" alt="Meiji Logo" width="120px" />
                               @endif
+                              <br>
+                              <br>
                               <div class="card-text">
                                   <p>
                                       Semua materi yang tertera di list produk PT Meiji Indonesia adalah untuk memberikan informasi yang benar dan tepat pada pengunjung website sesuai dengan latar belakang masing-masing.
+
+                                      Penyalahgunaan kegiatan dari materi yang tertera di Website ini merupakan tanggung jawab pribadi masing-masing dari pengunjung. Dengan ini pengunjung menyatakan bersedia dimintakan keterangan oleh pihak yang berwajib bila diperlukan dan siap diproses sesuai Hukum dan Peraturan Perundang-Undangan yang berlaku di Indonesia.
+
+                                      Apabila ada pihak yang memberikan keterangan yang tidak sesuai dengan hal yang dimaksud diatas, maka PT Meiji Indonesia tidak bertanggungjawab atas dampak yang ditimbulkan baik terhadap diri sendiri maupun kepada masyarakat luas.
                                   </p>
                               </div>
                           </div>
@@ -137,31 +146,20 @@
 @push('js')
 <script>
   document.addEventListener('DOMContentLoaded', function() {
-      var disclaimerModal = new bootstrap.Modal(document.getElementById('disclaimerModal'));
-
       // Tampilkan modal saat halaman dimuat
+      var disclaimerModal = new bootstrap.Modal(document.getElementById('disclaimerModal'));
       disclaimerModal.show();
-
-      // Tambahkan blur pada background
-      document.body.classList.add('modal-blur-background');
-
+      
       // Cek apakah sudah pernah setuju sebelumnya (opsional)
       var hasAgreed = localStorage.getItem('disclaimerAgreed');
       if (hasAgreed) {
           // Jika sudah pernah setuju, tidak perlu menampilkan modal
           disclaimerModal.hide();
-          document.body.classList.remove('modal-blur-background');
       }
-
-      // Ketika tombol agree diklik, simpan status di localStorage dan hapus blur
+      
+      // Ketika tombol agree diklik, simpan status di localStorage (opsional)
       document.getElementById('agreeButton').addEventListener('click', function() {
           localStorage.setItem('disclaimerAgreed', 'true');
-          document.body.classList.remove('modal-blur-background'); // Hapus blur saat setuju
-      });
-
-      // Ketika modal ditutup, hapus blur
-      disclaimerModal._element.addEventListener('hidden.bs.modal', function () {
-          document.body.classList.remove('modal-blur-background');
       });
   });
 </script>

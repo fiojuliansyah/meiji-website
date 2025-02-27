f037659506afe15753ee3d13d001e1acd47b06b9
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-02-27 21:20:54 +0700

    Update category.blade.php

--- resources/views/frontpage/news/category.blade.php
+++ resources/views/frontpage/news/category.blade.php
@@ -5,73 +5,84 @@
 @endsection
 
 @section('content')
-<section class="blog blog-grid" id="blog">
-    <div class="container">
-      <div class="row">
-        @forelse ($news as $item)
-            <div class="col-12 col-md-6 col-lg-4">
-            <div class="blog-entry" data-hover="">
-                <div class="entry-img">
-                <div class="entry-date">
-                    <div class="entry-content"><span class="day">{{ $item->created_at->format('d') }}</span><span class="month">{{ $item->created_at->format('M') }}</span><span class="year">{{ $item->created_at->format('Y') }}</span></div>
-                </div>
-                <!-- End .entry-date--><a href="{{ route('frontpage.news.show', [
-                                'lang' => app()->getLocale(),
-                                'category_slug' => $category->getTranslation('slug', app()->getLocale()),
-                                'news_slug' => $item->getTranslation('slug', app()->getLocale())
-                            ]) }}"><img src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->getTranslation('name', app()->getLocale()) }}" style="width: 100%; height: 100%; object-fit: cover; transition: transform 0.3s ease;"/></a>
-                </div>
-                <!-- End .entry-img-->
-                <div class="entry-content">
-                <div class="entry-meta">
-                    <div class="entry-category"><a href="{{ route('frontpage.news.show', [
-                        'lang' => app()->getLocale(),
-                        'category_slug' => $category->getTranslation('slug', app()->getLocale()),
-                        'news_slug' => $item->getTranslation('slug', app()->getLocale())
-                    ]) }}"><img src="{{ asset('storage/' . $item->image) }}">{{ $category->getTranslation('name', app()->getLocale()) }}</a>
-                    </div>
-                    <div class="divider"></div>
-                    <div class="entry-author"> 
-                    <p>Meiji Indonesia</p>
-                    </div>
-                </div>
-                <div class="entry-title">
-                    <h4>
-                        <a href="{{ route('frontpage.news.show', [
-                            'lang' => app()->getLocale(),
-                            'category_slug' => $category->getTranslation('slug', app()->getLocale()),
-                            'news_slug' => $item->getTranslation('slug', app()->getLocale())
-                        ]) }}">
-                            {!! Illuminate\Support\Str::limit(strip_tags($item->getTranslation('name', app()->getLocale())), 40) !!}
-                        </a>
-                    </h4>
-                </div>
-                <div class="entry-bio">
-                    <p>{!! Illuminate\Support\Str::limit(strip_tags($item->getTranslation('content', app()->getLocale())), 80) !!}</p>
-                </div>
-                <div class="entry-more"> <a class="btn btn--white btn-line btn-line-before btn-line-inversed"
-                        href="{{ route('frontpage.news.show', [
-                            'lang' => app()->getLocale(),
-                            'category_slug' => $category->getTranslation('slug', app()->getLocale()),
-                            'news_slug' => $item->getTranslation('slug', app()->getLocale())
-                        ]) }}">
-                        <div class="line"><span></span></div>
-                        <span>{{ translate('Read More') }}</span>
-                    </a>
-                </div>
+    <section class="blog blog-grid" id="blog">
+        <div class="container">
+            <div class="row">
+                @forelse ($news as $item)
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="blog-entry" data-hover="">
+                            <div class="entry-img">
+                                <div class="entry-date">
+                                    <div class="entry-content"><span
+                                            class="day">{{ $item->created_at->format('d') }}</span><span
+                                            class="month">{{ $item->created_at->format('M') }}</span><span
+                                            class="year">{{ $item->created_at->format('Y') }}</span></div>
+                                </div>
+                                <!-- End .entry-date-->
+                                <a
+                                    href="{{ route('frontpage.news.show', [
+                                        'lang' => app()->getLocale(),
+                                        'category_slug' => $category->getTranslation('slug', app()->getLocale()),
+                                        'news_slug' => $item->getTranslation('slug', app()->getLocale()),
+                                    ]) }}"><img
+                                        src="{{ asset('storage/' . $item->image) }}"
+                                        alt="{{ $item->getTranslation('name', app()->getLocale()) }}" /></a>
+                            </div>
+                            <!-- End .entry-img-->
+                            <div class="entry-content">
+                                <div class="entry-meta">
+                                    <div class="entry-category"><a
+                                            href="{{ route('frontpage.news.show', [
+                                                'lang' => app()->getLocale(),
+                                                'category_slug' => $category->getTranslation('slug', app()->getLocale()),
+                                                'news_slug' => $item->getTranslation('slug', app()->getLocale()),
+                                            ]) }}"><img
+                                                src="{{ asset('storage/' . $item->image) }}">{{ $category->getTranslation('name', app()->getLocale()) }}</a>
+                                    </div>
+                                    <div class="divider"></div>
+                                    <div class="entry-author">
+                                        <p>Meiji Indonesia</p>
+                                    </div>
+                                </div>
+                                <div class="entry-title">
+                                    <h4>
+                                        <a
+                                            href="{{ route('frontpage.news.show', [
+                                                'lang' => app()->getLocale(),
+                                                'category_slug' => $category->getTranslation('slug', app()->getLocale()),
+                                                'news_slug' => $item->getTranslation('slug', app()->getLocale()),
+                                            ]) }}">
+                                            {!! Illuminate\Support\Str::limit(strip_tags($item->getTranslation('name', app()->getLocale())), 40) !!}
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="entry-bio">
+                                    <p>{!! Illuminate\Support\Str::limit(strip_tags($item->getTranslation('content', app()->getLocale())), 80) !!}</p>
+                                </div>
+                                <div class="entry-more"> <a
+                                        class="btn btn--white btn-line btn-line-before btn-line-inversed"
+                                        href="{{ route('frontpage.news.show', [
+                                            'lang' => app()->getLocale(),
+                                            'category_slug' => $category->getTranslation('slug', app()->getLocale()),
+                                            'news_slug' => $item->getTranslation('slug', app()->getLocale()),
+                                        ]) }}">
+                                        <div class="line"><span></span></div>
+                                        <span>{{ translate('Read More') }}</span>
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- End .entry-content-->
+                        </div>
+                    @empty
+                        <div class="col-12 text-center">
+                            <p>{{ translate('No news found in this category.') }}</p>
+                        </div>
+                @endforelse
             </div>
-            <!-- End .entry-content-->
-            </div>
-        @empty
-            <div class="col-12 text-center">
-                <p>{{ translate('No news found in this category.') }}</p>
-            </div>
-        @endforelse
-      </div>
-      <!-- End .row-->
-      {{ $news->links() }}
-      <!-- End .row-->
-    </div>
-    <!-- End .container-->
-  </section>
+            <!-- End .row-->
+            {{ $news->links() }}
+            <!-- End .row-->
+        </div>
+        <!-- End .container-->
+    </section>
 @endsection

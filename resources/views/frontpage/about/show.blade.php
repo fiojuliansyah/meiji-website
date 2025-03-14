cc7648c258077532845a61244991bf0a9bf26aab
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-03-14 14:09:23 +0700

    Update show.blade.php

--- resources/views/frontpage/about/show.blade.php
+++ resources/views/frontpage/about/show.blade.php
@@ -3,29 +3,55 @@
 @section('title')
     {{ $about->getTranslation('title', app()->getLocale()) }} - Meiji Indonesia
 @endsection
+
 @section('content')
+
 <section class="team-single team-single-standard">
     <div class="container"> 
       <div class="row"> 
         <div class="col-12 col-lg-4">
-            <div class="sidebar sidebar-doctors">
-                <div class="widget widget-download">
-                    <div class="widget-content">
-                      <ul class="list-unstyled">
-                          @foreach ($abouts as $otherAbout)
-                            @if ($otherAbout->id != $about->id)   
-                              <li>
-                                <a href="{{ route('frontpage.about.show', ['lang' => app()->getLocale(), 'slug' => $otherAbout->getTranslation('slug', app()->getLocale())]) }}">
-                                  <span>{{ $otherAbout->getTranslation('title', app()->getLocale()) }}</span>
-                                </a>
-                              </li>
-                            @endif   
-                          @endforeach
-                      </ul>
+            <div class="sidebar sidebar-doctors sidebar-doctors-3">
+                <div class="team-modern team-modern-3">
+                  <div class="team-member">
+                    <div class="team-member-holder">
+                      <div class="team-img"><a class="link" href="javascript:void(0)"></a><img src="{{ asset('storage/' . $general->breadcrumb) }}" alt="member"/></div>
+                      <!-- End .team-img-->
+                      <div class="team-content-holder">
+                        <div class="team-content"> 
+                          <div class="team-title">
+                            <h4><a href="javascript:void(0)">Richard Muldoone</a></h4>
+                          </div>
+                          <div class="team-cat"><a href="javascript:void(0)">PT. Meiji Indonesian
+                            Pharmaceutical Industries</a></div>
+                          <div class="team-desc">
+                            {!! $general->getTranslation('bio', app()->getLocale()) !!}                        
+                          </div>
+                          <div class="team-social"> <a href="javascript:void(0)"><i class="fab fa-facebook-f"> </i></a><a href="javascript:void(0)"><i class="fab fa-twitter"></i></a><a href="javascript:void(0)"><i class="fas fa-envelope"></i></a><a href="javascript:void(0)"><i class="fas fa-phone-alt"></i></a></div>
+                        </div>
+                        <!-- End .team-content-->
+                      </div>
                     </div>
+                  </div>
                 </div>
-                <!-- End .widget-download-->
-            </div>
+                <div class="sidebar sidebar-doctors">
+                    <div class="widget widget-download">
+                        <div class="widget-content">
+                          <ul class="list-unstyled">
+                              @foreach ($abouts as $otherAbout)
+                                @if ($otherAbout->id != $about->id)   
+                                  <li>
+                                    <a href="{{ route('frontpage.about.show', ['lang' => app()->getLocale(), 'slug' => $otherAbout->getTranslation('slug', app()->getLocale())]) }}">
+                                      <span>{{ $otherAbout->getTranslation('title', app()->getLocale()) }}</span>
+                                    </a>
+                                  </li>
+                                @endif   
+                              @endforeach
+                          </ul>
+                        </div>
+                    </div>
+                    <!-- End .widget-download-->
+                </div>
+              </div>
             <!-- End .sidebar-->
         </div>
         <div class="col-12 col-lg-8">

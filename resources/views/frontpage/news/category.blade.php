d239723554d1d70592855226b3ad32062d98b8e3
Fio Juliansyah <63538861+fiojuliansyah@users.noreply.github.com>
2025-03-29 05:53:19 +0700

    Update category.blade.php

--- resources/views/frontpage/news/category.blade.php
+++ resources/views/frontpage/news/category.blade.php
@@ -5,7 +5,6 @@
 @endsection
 
 @section('content')
-<section class="page-title page-title-4" id="page-title">
     <div class="container">
         <div class="row" style="padding-bottom: 80px;">
             <div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-0">
@@ -131,7 +130,7 @@
             100% { left: 100%; }
         }
     </style>
-</section>
+
     <section class="blog blog-grid" id="blog">
         <div class="container">
             <div class="row">
